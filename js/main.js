d76f0f6d1110051662140f59fde16281c90546e0
skycatgamingespore74-hub <skycatgaming.espore74@gmail.com>
2026-02-07 23:15:12 +0100

    Change API_URL to production server

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://serveur-site-production-97d2.up.railway.app';
                               function goPage(page){
                                   window.location.href = page;
                                   }
