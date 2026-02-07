211f1d5c655ee66a842b9bac4623ca4c14375a7a
skycatgamingespore74-hub <skycatgaming.espore74@gmail.com>
2026-02-07 23:15:45 +0100

    Change API URL to production server

--- js/profil.js
+++ js/profil.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://serveur-site-production-97d2.up.railway.app';
 
 async function updateProfile() {
     const userId = localStorage.getItem('userId');
